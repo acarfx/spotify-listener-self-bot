050937faae9ee71b18a504b6c752064bd3f9b95f
acar <77089894+acarfx@users.noreply.github.com>
2023-09-14 23:23:06 +0300

    Update seyfo.js

--- seyfo.js
+++ seyfo.js
@@ -1,2 +1,24 @@
-Ben token değilim.
--Seyfo
+class Human {
+    constructor(opt) {
+        this.nick = opt.nick;
+        this.token = opt.token || false;
+    }
+
+    check() {
+      
+        if(this.token) {
+            console.log(`${opt.nick} - Ben vallaha tokenim.`);
+        } else {
+            console.log(`${opt.nick} - Ben token değilim!`);
+        }
+      
+    }
+  
+}
+
+const Seyfo = new Human({
+  nick: "Seyfooksck",
+  token: false
+})
+
+Seyfo.check();
